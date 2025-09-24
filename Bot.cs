9c879174fc038062aa1eb148663697007a510c02
Invra <identificationsucks@gmail.com>
2025-09-25 01:09:39 +1000

    Add: some messages and logging

--- Bot.cs
+++ Bot.cs
@@ -9,30 +9,33 @@ namespace TerryDavis {
     private readonly CommandHandler _commandHandler;
     private readonly BotConfig _config;
 
+
     public Bot() {
+      _config = new BotConfig();
+      Console.WriteLine($"\x1b[1;36m[STDOUT/status]:\x1b[0m Initialising the Discord bot");
       _client = new DiscordSocketClient(
-          new DiscordSocketConfig {
-            AlwaysDownloadUsers = true,
-            MessageCacheSize = 100,
-            GatewayIntents =
-                  GatewayIntents.Guilds
-                  | GatewayIntents.GuildMembers
-                  | GatewayIntents.GuildMessageReactions
-                  | GatewayIntents.GuildMessages
-                  | GatewayIntents.MessageContent
-                  | GatewayIntents.GuildVoiceStates,
+        new DiscordSocketConfig {
+          AlwaysDownloadUsers = true,
+          MessageCacheSize = 100,
+          GatewayIntents = GatewayIntents.Guilds
+            | GatewayIntents.GuildMembers
+            | GatewayIntents.GuildMessageReactions
+            | GatewayIntents.GuildMessages
+            | GatewayIntents.MessageContent
+            | GatewayIntents.GuildVoiceStates,
           }
       );
 
-      _config = new BotConfig();
       _commandHandler = new CommandHandler(_client, _config);
+
+       _client.Ready += () =>  {
+        Console.WriteLine($"\x1b[1;36m[STDOUT/status]:\x1b[0m Bot is online");
+        return Task.CompletedTask;
+      };
     }
 
     public async Task RunAsync() {
-      string token =
-          Environment.GetEnvironmentVariable("DISCORD_TOKEN")
-          ?? throw new InvalidOperationException("Missing DISCORD_TOKEN in .env");
-
+      string token = Environment.GetEnvironmentVariable("DISCORD_TOKEN")!;
       _client.Log += LogAsync;
       await _client.LoginAsync(TokenType.Bot, token);
       await _client.StartAsync();
@@ -42,8 +45,10 @@ namespace TerryDavis {
       await Task.Delay(-1);
     }
 
-    private Task LogAsync(LogMessage msg) {
-      Console.WriteLine(msg);
+    private Task LogAsync(LogMessage message) {
+      if (message.Severity != LogSeverity.Info) {
+        Console.WriteLine($"\x1b[1;93m[STDOUT/{message.Severity}]:\x1b[0;33m {message.Message}\x1b[0m");
+      }
       return Task.CompletedTask;
     }
   }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using System.Net.NetworkInformation;
+
 namespace TerryDavis {
   class Program {
     static async Task Main(string[] args) {
+      bool ErrState = false;
+
       // Print non-intrusive warning. As running this as admin
       // in the future is a really dumb idea.
       if (Environment.IsPrivilegedProcess) {
@@ -9,7 +13,36 @@ namespace TerryDavis {
         Console.ResetColor();
       }
 
+      Console.WriteLine($"\x1b[1;36m[STDOUT/status]:\x1b[0m Creating instance");
       DotNetEnv.Env.Load();
+      if (Environment.GetEnvironmentVariable("DISCORD_TOKEN") == null) {
+        Console.WriteLine($"\x1b[1;31m[STDOUT/critical]:\x1b[0m No DISCORD_TOKEN variable found in the ENV");
+        ErrState = true;
+      };
+
+      if (Environment.GetEnvironmentVariable("BOT_PREFIX") == null) {
+        Console.WriteLine($"\x1b[1;31m[STDOUT/critical]:\x1b[0m No BOT_PREFIX variable found in the ENV");
+        ErrState = true;
+      };
+
+      if (ErrState) {
+        Console.WriteLine($"\x1b[1;31m[STDOUT/critical]:\x1b[0;31m There have been runtime errors. Please solve them to be able to continue\x1b[0m");
+        return;
+      }
+
+      Console.WriteLine($"\x1b[1;36m[STDOUT/status]:\x1b[0m Testing network connectivity to Discord");
+      try {
+        Ping myPing = new Ping();
+        String host = "discord.com";
+        byte[] buffer = new byte[32];
+        int timeout = 1000;
+        PingOptions pingOptions = new PingOptions();
+        PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+        Console.WriteLine($"\x1b[1;36m[STDOUT/status]:\x1b[0m Networking test has passed");
+      } catch {
+        Console.WriteLine($"\x1b[1;36m[STDOUT/status]:\x1b[0m Cannot contact Discord, this is just a warning it may not connect.");
+      }
+
       var bot = new Bot();
       await bot.RunAsync();
     }
