49815e89a4096fc5a2832eac8cf94e4847691a3b
Invra <identificationsucks@gmail.com>
2025-09-25 03:58:10 +1000

    Add: help

---
+++ Commands/HelpCommand.cs
@@ -0,0 +1,41 @@
+using Discord;
+using Discord.Commands;
+
+namespace TerryDavis.Commands {
+  public class HelpCommand : ModuleBase<SocketCommandContext> {
+    private readonly CommandService _commands;
+
+    public HelpCommand(CommandService commands) {
+      _commands = commands;
+    }
+
+    [Command("help")]
+    [Summary("Shows a list of available commands.")]
+    public async Task HelpAsync() {
+      var embed = new EmbedBuilder()
+        .WithTitle("Commands List")
+        .WithColor(Color.Blue)
+        .WithFooter($"Requested by {Context.User.Username}")
+        .WithCurrentTimestamp();
+
+      foreach (var module in _commands.Modules) {
+        var commandDescriptions = new List<string>();
+
+        foreach (var cmd in module.Commands) {
+          var preconditionResult = await cmd.CheckPreconditionsAsync(Context);
+          if (preconditionResult.IsSuccess) {
+            commandDescriptions.Add(
+              $"{cmd.Summary ?? "No summary"}"
+            );
+          }
+        }
+
+        if (commandDescriptions.Count > 0) {
+          embed.AddField(module.Name, string.Join("\n", commandDescriptions));
+        }
+      }
+
+      await ReplyAsync(embed: embed.Build());
+    }
+  }
+}
