3d5c961992a0204e5cb79fa3110418895db5f2c3
Invra <identificationsucks@gmail.com>
2025-10-07 06:43:44 +1100

    Possibly fix issue

--- HardwareInfo/src/mods/windows.rs
+++ HardwareInfo/src/mods/windows.rs
@@ -1,11 +1,5 @@
-// TODO: Windows sucks ass and i cant do this correctly
+use winver::WindowsVersion;
 
-// use wmi::{COMLibrary, WMIConnection};
-
-// pub fn get_windows_caption() -> Option<String> {
-//   let com = COMLibrary::new().ok()?;
-//   let wmi_con = WMIConnection::new(com.into()).ok()?;
-//   let results: Vec<std::collections::HashMap<String, wmi::Variant>> =
-//     wmi_con.raw_query("SELECT Caption FROM Win32_OperatingSystem").ok()?;
-//   results.first()?.get("Caption")?.to_string().into()
-// }
+pub fn get_windows_caption() -> Option<String> {
+  WindowsVersion::detect().unwrap()
+}
