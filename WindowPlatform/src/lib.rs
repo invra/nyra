a6cf3a912f8c1358cdfce61bde4067e562301a65
Invra <identificationsucks@gmail.com>
2025-10-11 22:51:20 +1100

    Fix some stuff

--- WindowPlatform/src/lib.rs
+++ WindowPlatform/src/lib.rs
@@ -7,7 +7,9 @@
 
 mod theme;
 use {
-  gpui::{App, Rgba, WindowBounds, WindowOptions, div, point, prelude::*, px, size},
+  gpui::{
+    App, KeyBinding, Rgba, WindowBounds, WindowOptions, actions, div, point, prelude::*, px, size,
+  },
   std::{
     ffi::{CStr, CString},
     os::raw::c_char,
@@ -75,25 +77,41 @@ impl gpui::Render for NyraView {
       .flex_col()
       .size_full()
       .bg(self.colors.bg)
-      .items_center()
-      .text_color(self.colors.text)
-      .child(div().child("Nyra").text_3xl())
       .child(
         div()
-          .id("start-bot")
-          .child("Start Bot")
-          .bg(self.colors.surface)
+          .w_full()
+          .h(px(40.0))
+          .border_b_1()
+          .border_color(self.colors.overlay)
+          .flex()
+          .items_center()
+          .justify_center()
           .text_color(self.colors.text)
-          .p(px(8.))
-          .border(px(1.))
-          .rounded(px(4.))
-          .on_click({
-            let gui = self.gui.clone();
-            move |_event, _cx, _| gui.lock().unwrap().start_bot()
-          }),
+          .child(div().child("Nyra").font_weight(gpui::FontWeight::SEMIBOLD)),
+      )
+      .child(
+        div().flex().flex_col().items_center().child(
+          div()
+            .id("start-bot")
+            .child("Start Bot")
+            .bg(self.colors.surface)
+            .text_color(self.colors.text)
+            .p(px(8.))
+            .border(px(1.))
+            .rounded(px(4.))
+            .mt_16()
+            .cursor_pointer()
+            .hover(|style| style.bg(self.colors.overlay))
+            .on_click({
+              let gui = self.gui.clone();
+              move |_event, _cx, _| gui.lock().unwrap().start_bot()
+            }),
+        ),
       )
   }
 }
+
+actions!(window, [Quit]);
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn init_gui(
@@ -110,13 +128,15 @@ pub unsafe extern "C" fn init_gui(
 
   gpui::Application::new().run(move |cx: &mut App| {
     let bounds = WindowBounds::Windowed(gpui::Bounds::centered(None, size(px(400.), px(200.)), cx));
+
     cx.open_window(
       WindowOptions {
         window_bounds: Some(bounds),
         titlebar: Some(gpui::TitlebarOptions {
           title: None,
+
           appears_transparent: true,
-          traffic_light_position: Some(point(px(9.0), px(9.0))),
+          traffic_light_position: Some(point(px(12.0), px(12.0))),
         }),
         window_min_size: Some(gpui::Size {
           width: px(360.0),
@@ -132,5 +152,7 @@ pub unsafe extern "C" fn init_gui(
       },
     )
     .unwrap();
+    cx.on_action(|_: &Quit, cx| cx.quit());
+    cx.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
   });
 }
