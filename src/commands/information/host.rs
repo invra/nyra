911cdcf664cb58fb5c785347ffb452fb386d4658
Invra <identificationsucks@gmail.com>
2025-10-17 15:55:35 +1100

    Add host.rs

---
+++ src/commands/information/host.rs
@@ -0,0 +1,47 @@
+/*
+  SPDX-License-Identifier: Unlicense
+  Project: Nyra
+  File: commands/information/ping.rs
+  Authors: Invra, Hiten-Tandon
+*/
+
+use {
+  crate::commands::helper::{Context, Error, MyCommand},
+  chrono::{DateTime, Utc},
+  poise::{
+    CreateReply, command,
+    serenity_prelude::{Colour, CreateEmbed, CreateEmbedFooter},
+  },
+};
+
+/// Ping command
+#[command(prefix_command, slash_command, category = "information")]
+pub async fn host(ctx: Context<'_>) -> Result<(), Error> {
+  let timestamp: DateTime<Utc> = chrono::offset::Utc::now();
+  let ping_time = ctx.ping().await;
+
+  let result = if ping_time.is_zero() {
+    "Unavailable".into()
+  } else {
+    format!("{:#.0?}", ping_time)
+  };
+
+  let reply = CreateReply::default().embed(
+    CreateEmbed::new()
+      .title("Gateway latency")
+      .field("Gateway Latency", result, false)
+      .footer(CreateEmbedFooter::new(format!(
+        "Test by {}",
+        ctx.author().name
+      )))
+      .timestamp(timestamp)
+      .color(Colour::PURPLE),
+  );
+
+  ctx.send(reply).await?;
+
+  Ok(())
+}
+inventory::submit! { MyCommand(host) }
+
+fn get_cpu_model() {}
