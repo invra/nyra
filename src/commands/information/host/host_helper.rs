638f65fe255c569e6498b83e2048f5e50da5c0a7
Hiten Tandon <tandonhiten@gamil.com>
2025-10-27 21:45:41 +0530

    Feat: Works on Linux

--- src/commands/information/host/host_helper.rs
+++ src/commands/information/host/host_helper.rs
@@ -1,3 +1,30 @@
+#[cfg(target_os = "linux")]
+pub fn get_cpu_model() -> Box<str> {
+  use std::{
+    collections::HashMap,
+    fs::File,
+    io::Read,
+  };
+
+  let mut buf = String::new();
+  if File::open("/proc/cpuinfo")
+    .and_then(|mut f| f.read_to_string(&mut buf))
+    .is_err()
+  {
+    return "".into();
+  }
+
+  buf
+    .lines()
+    .filter_map(|x| x.split_once(':'))
+    .map(|(a, b)| (a.trim(), b.trim()))
+    .collect::<HashMap<_, _>>()
+    .get("model name")
+    .copied()
+    .map(Into::into)
+    .unwrap_or_default()
+}
+
 #[cfg(target_os = "macos")]
 pub fn get_cpu_model() -> Box<str> {
   use sysctl::Sysctl;
@@ -45,6 +72,44 @@ pub fn get_cpu_model() -> Box<str> {
 
 pub fn get_cpu_count() -> usize {
   num_cpus::get()
+}
+
+#[cfg(target_os = "linux")]
+pub fn get_mem() -> (f64, f64) {
+  use std::{
+    collections::HashMap,
+    fs::File,
+    io::Read,
+  };
+
+  let mut buf = String::new();
+  if File::open("/proc/meminfo")
+    .and_then(|mut f| f.read_to_string(&mut buf))
+    .is_err()
+  {
+    return (0.0, 0.0);
+  }
+
+  let data = buf
+    .lines()
+    .filter_map(|x| x.split_once(':'))
+    .collect::<HashMap<_, _>>();
+
+  let total = data
+    .get("MemTotal")
+    .map(|s| s.trim_matches(|x: char| !x.is_ascii_digit()).parse::<f64>())
+    .and_then(Result::ok)
+    .map(|x| x / 2_f64.powi(20))
+    .unwrap_or_default();
+
+  let used = data
+    .get("MemAvailable")
+    .map(|s| s.trim_matches(|x: char| !x.is_ascii_digit()).parse::<f64>())
+    .and_then(Result::ok)
+    .map(|x| total - x / 2_f64.powi(20))
+    .unwrap_or_default();
+
+  (used, total)
 }
 
 #[cfg(target_os = "macos")]
@@ -220,12 +285,10 @@ pub fn get_os_name() -> Box<str> {
     return "Unknown Linux".into();
   }
 
-  let os_data = buf
+  let pretty = buf
     .lines()
     .filter_map(|x| x.split_once('='))
-    .collect::<HashMap<_, _>>();
-
-  let pretty = os_data
+    .collect::<HashMap<_, _>>()
     .get("PRETTY_NAME")
     .map(|s| s.trim_matches('"'))
     .unwrap_or("Unknown Linux");
